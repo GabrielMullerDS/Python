6c50698ca6e911c112ecbd8de8da64d69b363611
GabrielMüllerDS <61920654+GabrielMullerDS@users.noreply.github.com>
2022-03-18 20:18:56 -0300

    Create aula1.py

---
+++ aula1.py
@@ -0,0 +1,3 @@
+altura, peso = float(input("Digite sua altura: ")), float(input("Digite seu peso: "))
+IMC = peso/(altura*altura)
+print("Baseado na altura {} e no peso de {} seu IMC é de aproximadamente {:.2f}.".format(altura, peso, IMC)))
