008359ddf0118b196ecfd60c74d93df4edd726cb
GabrielMüllerDS <61920654+GabrielMullerDS@users.noreply.github.com>
2022-03-18 20:20:44 -0300

    Create aula3.py

---
+++ aula3.py
@@ -0,0 +1,29 @@
+#if, elif, else
+print("Escolha um mês pelo seu número: (De 1 à 12)\n")
+Num = int(input())
+if Num == 1:
+    print("Você escolheu o mês de Janeiro.")
+elif Num == 2:
+    print("Você escolheu o mês de Fevereiro.")
+elif Num == 3:
+    print("Você escolheu o mês de Março.")
+elif Num == 4:
+    print("Você escolheu o mês de Abril.")
+elif Num == 5:
+    print("Você escolheu o mês de Maio.")
+elif Num == 6:
+    print("Você escolheu o mês de Junho.")
+elif Num == 7:
+    print("Você escolheu o mês de Julho.")
+elif Num == 8:
+    print("Você escolheu o mês de Agosto.")
+elif Num == 9:
+    print("Você escolheu o mês de Setembro.")
+elif Num == 10:
+    print("Você escolheu o mês de Outubro.")
+elif Num == 11:
+    print("Você escolheu o mês de Novembro.")
+elif Num == 12:
+    print("Você escolheu o mês de Desembro.")
+else:
+    print("Eu te disse para escolher entre 1 e 12, ser humano burro.")
